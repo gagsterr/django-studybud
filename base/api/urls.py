334d0dcbfdefd787c58b44cbfb1551fd1606b4e1
Gagan Saluja <gagansaluja@192.168.1.18>
2022-06-15 16:55:27 +0530

    First build

---
+++ base/api/urls.py
@@ -0,0 +1,10 @@
+import imp
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.getRoutes),
+    path('rooms/', views.getRooms),
+    path('rooms/<str:pk>/', views.getRoom),
+
+]
